17b0aeecc9b1d8a90853672951c36a159d711d14
kasrasaeed <kasrasaeed@outlook.com>
2022-03-22 17:33:43 +0430

    feat: some error msg added

--- entity/error.go
+++ entity/error.go
@@ -5,3 +5,5 @@ import "errors"
 var InvalidUserEntity = errors.New("user info can not be empty")
 
 var InvalidNewsEntity = errors.New("news info can not be empty")
+
+var NotFound = errors.New("not found")
