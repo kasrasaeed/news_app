f3ea396491eb3889a399ce6de4c57dcd0286d024
kasrasaeed <kasrasaeed@outlook.com>
2022-03-22 17:34:29 +0430

    feat: user usecase created

--- usecase/user/interface.go
+++ usecase/user/interface.go
@@ -1,1 +1,29 @@
 package user
+
+import "github.com/kasrasaeed/news_app/entity"
+
+type Reader interface {
+	Get(id uint32) (*entity.User, error)
+	Search(query string) ([]*entity.User, error)
+	GetAll() ([]*entity.User, error)
+}
+
+type Writer interface {
+	Create(e *entity.User) (uint32, error)
+	Update(e *entity.User) error
+	Delete(id uint32) error
+}
+
+type Repository interface {
+	Reader
+	Writer
+}
+
+type UseCase interface {
+	GetUser(id uint32) (*entity.User, error)
+	SearchUser(query string) ([]*entity.User, error)
+	ListUsers() ([]*entity.User, error)
+	CreateUser(username, password, role string) (uint32, error)
+	UpdateUser(e *entity.User) error
+	DeleteUser(id uint32) error
+}
--- usecase/user/service.go
+++ usecase/user/service.go
@@ -1,1 +1,55 @@
 package user
+
+import (
+	"github.com/kasrasaeed/news_app/entity"
+	"strings"
+	"time"
+)
+
+type Service struct {
+	repo Repository
+}
+
+func NewService(r Repository) *Service {
+	return &Service{repo: r}
+}
+
+func (s *Service) CreateUser(username, password, role string) (uint32, error) {
+	u, err := entity.NewUser(role, username, password)
+	if err != nil {
+		return 0, err
+	}
+	return s.repo.Create(u)
+}
+
+func (s *Service) GetUser(id uint32) (*entity.User, error) {
+	return s.repo.Get(id)
+}
+
+func (s *Service) SearchUser(query string) ([]*entity.User, error) {
+	return s.repo.Search(strings.ToLower(query))
+}
+
+func (s *Service) ListUsers() ([]*entity.User, error) {
+	return s.repo.GetAll()
+}
+
+func (s *Service) DeleteUser(id uint32) error {
+	u, err := s.GetUser(id)
+	if u != nil {
+		return entity.NotFound
+	}
+	if err != nil {
+		return err
+	}
+	return s.repo.Delete(id)
+}
+
+func (s *Service) UpdateUser(e *entity.User) error {
+	err := e.Validate()
+	if err != nil {
+		return entity.InvalidUserEntity
+	}
+	e.UpdatedAt = time.Now()
+	return s.repo.Update(e)
+}
