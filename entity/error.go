b433501d4820ced0d6f6448133c372ed43187cbb
kasrasaeed <kasrasaeed@outlook.com>
2022-03-22 15:18:47 +0430

    feat: error message added also added methods to user entity

---
+++ entity/error.go
@@ -0,0 +1,5 @@
+package entity
+
+import "errors"
+
+var InvalidUserEntity = errors.New("user info can not be empty")
--- entity/user.go
+++ entity/user.go
@@ -1,5 +1,50 @@
 package entity
 
+import (
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
+
 type User struct {
-	Id uint32
+	CreatedDate time.Time
+	Id          uint32
+	LastVisit   time.Time
+	Role        string
+	UserName    string
+	PassWord    string
+}
+
+func NewUser(lastVisit time.Time, id uint32, role, userName, Password string) (*User, error) {
+	user := &User{
+		CreatedDate: time.Now(),
+		Id:          id,
+		LastVisit:   lastVisit,
+		Role:        role,
+		UserName:    userName,
+	}
+	pwd, err := hashPassWord(Password)
+	if err != nil {
+		return nil, err
+	}
+	user.PassWord = pwd
+	err = user.validate()
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (u *User) validate() error {
+	if u.UserName == "" || u.PassWord == "" {
+		return InvalidUserEntity
+	}
+	return nil
+}
+
+func hashPassWord(rawPass string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(rawPass), 10)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
 }
