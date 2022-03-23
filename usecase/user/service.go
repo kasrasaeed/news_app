28149910e0663afe5ec4f9d364d44dd41ff45883
kasrasaeed <kasrasaeed@outlook.com>
2022-03-23 20:29:37 +0430

    feat: lastvisit method added

--- usecase/user/service.go
+++ usecase/user/service.go
@@ -1,10 +1,14 @@
 package user
 
 import (
-	"github.com/kasrasaeed/news_app/entity"
+	"github.com/kasrasaeed/news_app/domain/entity"
+	"github.com/kasrasaeed/news_app/domain/repository/user"
+	"github.com/kasrasaeed/news_app/pkg/id"
 	"strings"
 	"time"
 )
+
+type Repository = user.Repository
 
 type Service struct {
 	repo Repository
@@ -14,16 +18,16 @@ func NewService(r Repository) *Service {
 	return &Service{repo: r}
 }
 
-func (s *Service) CreateUser(username, password, role string) (uint32, error) {
+func (s *Service) CreateUser(username, password, role string) (id.UUID, error) {
 	u, err := entity.NewUser(role, username, password)
 	if err != nil {
-		return 0, err
+		return u.Id, err
 	}
 	return s.repo.Create(u)
 }
 
-func (s *Service) GetUser(id uint32) (*entity.User, error) {
-	return s.repo.Get(id)
+func (s *Service) GetUser(id id.UUID) (*entity.User, error) {
+	return s.repo.GetById(id)
 }
 
 func (s *Service) SearchUser(query string) ([]*entity.User, error) {
@@ -34,7 +38,7 @@ func (s *Service) ListUsers() ([]*entity.User, error) {
 	return s.repo.GetAll()
 }
 
-func (s *Service) DeleteUser(id uint32) error {
+func (s *Service) DeleteUser(id id.UUID) error {
 	u, err := s.GetUser(id)
 	if u != nil {
 		return entity.NotFound
@@ -51,5 +55,13 @@ func (s *Service) UpdateUser(e *entity.User) error {
 		return entity.InvalidUserEntity
 	}
 	e.UpdatedAt = time.Now()
+	return s.repo.Update(e)
+}
+
+func (s *Service) UpdateLastVisit(e *entity.User) error {
+	err := entity.UpdateLastVisit(e)
+	if err != nil {
+		return err
+	}
 	return s.repo.Update(e)
 }
