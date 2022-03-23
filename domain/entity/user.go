8807ce5208ac7a96ca0becec0fa4e7884c0f8093
kasrasaeed <kasrasaeed@outlook.com>
2022-03-23 20:27:32 +0430

    fix: user file directory moved

--- domain/entity/user.go
+++ domain/entity/user.go
@@ -1,25 +1,28 @@
 package entity
 
 import (
+	"github.com/kasrasaeed/news_app/pkg/id"
 	"golang.org/x/crypto/bcrypt"
 	"time"
 )
 
 type User struct {
 	CreatedDate time.Time
-	Id          uint32
+	Id          id.UUID
 	LastVisit   time.Time
 	PassWord    string
 	Role        string
-	UpdatedAt   time.Time
 	UserName    string
+	UpdatedAt   time.Time
 }
 
 func NewUser(role, userName, Password string) (*User, error) {
 	user := &User{
+		Id:          id.NewUUID(),
 		CreatedDate: time.Now(),
 		Role:        role,
 		UserName:    userName,
+		LastVisit:   time.Now(),
 	}
 	pwd, err := hashPassWord(Password)
 	if err != nil {
@@ -31,6 +34,16 @@ func NewUser(role, userName, Password string) (*User, error) {
 		return nil, err
 	}
 	return user, nil
+}
+
+func UpdateLastVisit(u *User) error {
+	updatedUser := u
+	updatedUser.LastVisit = time.Now()
+	err := updatedUser.Validate()
+	if err != nil {
+		return InvalidUserEntity
+	}
+	return nil
 }
 
 func (u *User) Validate() error {
