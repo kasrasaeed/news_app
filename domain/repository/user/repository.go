2f99f22fad2571c88d5e07865c59cd93ea31f197
kasrasaeed <kasrasaeed@outlook.com>
2022-03-23 20:30:17 +0430

    feat: repository.go added for enterprise business logic rules

---
+++ domain/repository/user/repository.go
@@ -0,0 +1,24 @@
+package user
+
+import (
+	"github.com/kasrasaeed/news_app/domain/entity"
+	"github.com/kasrasaeed/news_app/pkg/id"
+)
+
+type Reader interface {
+	GetById(id id.UUID) (*entity.User, error)
+	Search(query string) ([]*entity.User, error)
+	GetAll() ([]*entity.User, error)
+}
+
+type Writer interface {
+	Create(e *entity.User) (id.UUID, error)
+	Update(e *entity.User) error
+	UpdateLastVisit(e *entity.User) error
+	Delete(id id.UUID) error
+}
+
+type Repository interface {
+	Reader
+	Writer
+}
