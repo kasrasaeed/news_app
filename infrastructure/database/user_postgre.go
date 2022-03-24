76064fe91be95a5de5e51255a872613c38346e57
kasrasaeed <kasrasaeed@outlook.com>
2022-03-24 22:34:22 +0430

    feat: GetByName method added

--- infrastructure/database/user_postgre.go
+++ infrastructure/database/user_postgre.go
@@ -2,8 +2,10 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/kasrasaeed/news_app/domain/entity"
 	"github.com/kasrasaeed/news_app/pkg/id"
+	"strings"
 )
 
 type userPostgre struct {
@@ -12,33 +14,6 @@ type userPostgre struct {
 
 func NewUserPostgre(db *sql.DB) *userPostgre {
 	return &userPostgre{db: db}
-}
-
-func (up *userPostgre) Create(e *entity.User) (id.UUID, error) {
-	rawQuery := `insert into users (id, user_name, pass_word, role, last_visit, created_date, updated_date)
-				 values (?,?,?,?,?,?,?)`
-	stmt, err := up.db.Prepare(rawQuery)
-	if err != nil {
-		return e.Id, err
-	}
-
-	_, err = stmt.Exec(
-		e.Id,
-		e.UserName,
-		e.PassWord,
-		e.Role,
-		e.LastVisit,
-		e.CreatedDate.Format("2020-01-01"),
-		e.UpdatedAt.Format("2020-01-01"),
-	)
-	if err != nil {
-		return e.Id, err
-	}
-	err = stmt.Close()
-	if err != nil {
-		return e.Id, err
-	}
-	return e.Id, nil
 }
 
 func (up *userPostgre) GetById(id id.UUID) (*entity.User, error) {
@@ -54,4 +29,63 @@ func (up *userPostgre) GetById(id id.UUID) (*entity.User, error) {
 		return nil, err
 	}
 	return &user, nil
+}
+
+func (up *userPostgre) GetByName(name string) (*entity.User, error) {
+	rawQuery := `select from users where user_name = ?`
+	stmt, err := up.db.Prepare(rawQuery)
+	if err != nil {
+		return nil, err
+	}
+	row := stmt.QueryRow(name)
+
+	var user *entity.User
+	err = row.Scan(
+		user.Id,
+		user.UserName,
+		user.PassWord,
+		user.Role,
+		user.LastVisit,
+		user.CreatedDate,
+		user.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (up *userPostgre) Create(e *entity.User) (id.UUID, error) {
+	user, err := up.GetByName(strings.ToLower(e.UserName))
+	if err != nil {
+		return [16]byte{}, err
+	}
+	if user != nil {
+		return [16]byte{}, fmt.Errorf("this username already exists")
+	}
+	rawQuery := `insert into users (id, user_name, pass_word, role, last_visit, created_date, updated_date)
+				 values (?,?,?,?,?,?,?)`
+	stmt, err := up.db.Prepare(rawQuery)
+	if err != nil {
+		return e.Id, err
+	}
+
+	_, err = stmt.Exec(
+		e.Id,
+		strings.ToLower(e.UserName),
+		e.PassWord,
+		e.Role,
+		e.LastVisit,
+		e.CreatedDate.Format("2020-01-01"),
+		e.UpdatedAt.Format("2020-01-01"),
+	)
+	if err != nil {
+		return e.Id, err
+	}
+	err = stmt.Close()
+	if err != nil {
+		return e.Id, err
+	}
+	return e.Id, nil
 }
